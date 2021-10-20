125124c03c9a7f19cc2e0316b6b152b4b6c85bba
allyboy08 <alexsassman65@gmail.com>
2021-10-20 14:45:16 +0200

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,24 @@
 <template>
   <div class="home">
   <h1>home</h1>
-  <input type="text" v-model="search">
-  <p>search term - {{search}}</p>
-  <div v-for="name in matchingNames" :key="name">{{name}}</div>
-  <button @click="handleClick">stop watching</button>
+  <PostList :posts="posts" />
   </div>
 </template>
 
 <script>
-import { computed, ref, watch, watchEffect } from 'vue'
+import PostList from '../components/PostList.vue'
+import { ref } from 'vue'
 
 export default {
   name: 'Home',
+  components: {PostList},
   setup() {
-    const search = ref('')
-    const names = ref(['bob', 'ross', 'mario', 'yoshi', 'luigi', 'bowser', 'peach'])
+    const posts = ref([
+      {title: 'welcome to the blog', body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem fugiat vitae, et, modi, officiis ipsum alias magnam soluta animi quos laborum corrupti! Ab sunt esse at dignissimos laborum recusandae quaerat tempore, qui modi nam temporibus corrupti suscipit et expedita blanditiis quod, perferendis animi itaque harum delectus. Ipsa id, minus repellat rerum non doloremque laboriosam fugit amet enim deserunt quae sit cumque, ut omnis quisquam nihil, vel tempore praesentium est consequuntur corrupti consectetur tenetur excepturi. Minima, accusamus! Porro, laudantium excepturi. Deleniti, quia. Nam voluptatum enim assumenda vero obcaecati necessitatibus eaque ad consequatur dignissimos! Voluptatum, laboriosam vitae reprehenderit consectetur quasi odio magni.' , id: 1},
+      {title: 'top 5 ninja tools', body: 'Lorem ipsum', id: 2}
+    ])
 
-    const stopWatch = watch(search, () => {
-      console.log('watch ran')
-    })
-
-    const stopEffect = watchEffect(() => {
-      console.log('watch effect ran', search.value)
-    })
-
-    const matchingNames = computed(() => {
-      return names.value.filter((name) => name.includes(search.value))
-    })
-
-    const handleClick = () => {
-      stopWatch()
-      stopEffect()
-    }
-
-    return {names, search, matchingNames, handleClick}
+    return {posts}
   }
 }
 </script>
