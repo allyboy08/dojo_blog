db54ef34555735fde35d8173cca42fa4254ee5fa
allyboy08 <alexsassman65@gmail.com>
2021-10-21 13:28:10 +0200

    chanages

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="home">
   <h1>home</h1>
-  <PostList v-if="showPosts" :posts="posts" />
-  <button @click="showPosts = !showPosts">toggle posts</button>
-  <button @click="posts.pop()">delete post</button>
+  <div v-if="error">{{error}}</div>
+  <div v-if="posts.length">
+    <PostList :posts="posts" />
+  </div>
+  <div v-else>loading...</div>
+  
   </div>
 </template>
 
 <script>
 import PostList from '../components/PostList.vue'
-import { ref } from 'vue'
+import getPosts from '../composables/getPosts'
 
 export default {
   name: 'Home',
   components: {PostList},
   setup() {
-    const posts = ref([
-      {title: 'welcome to the blog', body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem fugiat vitae, et, modi, officiis ipsum alias magnam soluta animi quos laborum corrupti! Ab sunt esse at dignissimos laborum recusandae quaerat tempore, qui modi nam temporibus corrupti suscipit et expedita blanditiis quod, perferendis animi itaque harum delectus. Ipsa id, minus repellat rerum non doloremque laboriosam fugit amet enim deserunt quae sit cumque, ut omnis quisquam nihil, vel tempore praesentium est consequuntur corrupti consectetur tenetur excepturi. Minima, accusamus! Porro, laudantium excepturi. Deleniti, quia. Nam voluptatum enim assumenda vero obcaecati necessitatibus eaque ad consequatur dignissimos! Voluptatum, laboriosam vitae reprehenderit consectetur quasi odio magni.' , id: 1},
-      {title: 'top 5 ninja tools', body: 'Lorem ipsum', id: 2}
-    ])
-    const showPosts = ref(true)
-
-    return {posts, showPosts}
+    const {posts, error, load} = getPosts()
+    load()
+   
+    return {posts, error}
   }
 }
 </script>
