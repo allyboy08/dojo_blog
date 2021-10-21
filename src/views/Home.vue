5797028e19fe474ced30d826f0532d8a5c7cd93b
allyboy08 <alexsassman65@gmail.com>
2021-10-21 10:01:36 +0200

    chanages

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
   <h1>home</h1>
-  <PostList :posts="posts" />
+  <PostList v-if="showPosts" :posts="posts" />
+  <button @click="showPosts = !showPosts">toggle posts</button>
+  <button @click="posts.pop()">delete post</button>
   </div>
 </template>
 
@@ -17,8 +19,9 @@ export default {
       {title: 'welcome to the blog', body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem fugiat vitae, et, modi, officiis ipsum alias magnam soluta animi quos laborum corrupti! Ab sunt esse at dignissimos laborum recusandae quaerat tempore, qui modi nam temporibus corrupti suscipit et expedita blanditiis quod, perferendis animi itaque harum delectus. Ipsa id, minus repellat rerum non doloremque laboriosam fugit amet enim deserunt quae sit cumque, ut omnis quisquam nihil, vel tempore praesentium est consequuntur corrupti consectetur tenetur excepturi. Minima, accusamus! Porro, laudantium excepturi. Deleniti, quia. Nam voluptatum enim assumenda vero obcaecati necessitatibus eaque ad consequatur dignissimos! Voluptatum, laboriosam vitae reprehenderit consectetur quasi odio magni.' , id: 1},
       {title: 'top 5 ninja tools', body: 'Lorem ipsum', id: 2}
     ])
+    const showPosts = ref(true)
 
-    return {posts}
+    return {posts, showPosts}
   }
 }
 </script>
